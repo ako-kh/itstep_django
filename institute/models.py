6b7f19a6d398d1da9ccfd0ea39d95969e234a384
Akaki Khorava <xoravaakaki989@gmail.com>
2026-01-28 13:02:20 +0100

    add __str__ methods to models

--- institute/models.py
+++ institute/models.py
@@ -4,15 +4,23 @@ from django.db import models
 class Professor(models.Model):
     full_name = models.CharField(max_length=30)
     age = models.IntegerField()
-    pass
+
+    def __str__(self):
+        return self.full_name
 
 class Curse(models.Model):
     subject = models.CharField(max_length=30)
-    star_date = models.DateField()
+    start_date = models.DateField()
     end_date = models.DateField()
     professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.subject
 
 class Student(models.Model):
     full_name = models.CharField(max_length=30)
     age = models.IntegerField()
     curse = models.ForeignKey(Curse, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.full_name
