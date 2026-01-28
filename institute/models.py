1e97c7e0a7887f2bf90999c8f36f61349de27539
Akaki Khorava <xoravaakaki989@gmail.com>
2026-01-28 12:57:22 +0100

    changed model names to singular

--- institute/models.py
+++ institute/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 
-class Professors(models.Model):
+class Professor(models.Model):
     full_name = models.CharField(max_length=30)
     age = models.IntegerField()
     pass
 
-class Curses(models.Model):
+class Curse(models.Model):
     subject = models.CharField(max_length=30)
     star_date = models.DateField()
     end_date = models.DateField()
-    professor = models.ForeignKey(Professors, on_delete=models.SET_NULL, null=True)
+    professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, null=True)
 
-class Students(models.Model):
+class Student(models.Model):
     full_name = models.CharField(max_length=30)
     age = models.IntegerField()
-    curse = models.ForeignKey(Curses, on_delete=models.SET_NULL, null=True)
+    curse = models.ForeignKey(Curse, on_delete=models.SET_NULL, null=True)
