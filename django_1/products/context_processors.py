025ee9ab2d5b21ef1e8ccb813f609e135454d6d6
Akaki Khorava <xoravaakaki989@gmail.com>
2026-02-08 20:47:00 +0100

    add last five items in global context

--- django_1/products/context_processors.py
+++ django_1/products/context_processors.py
@@ -1,12 +1,14 @@
-from .models import Category
+from .models import Category, Product
 from django.db.models import Count
 
 def global_context(request):
     categories = Category.objects.filter(products__isnull=False).distinct()
     categories = categories.annotate(product_count=Count('products'))
+    last_five_products = Product.objects.all().order_by('-created_at')[:5]
 
     context = {
         'categories': categories,
+        'last_five_products': last_five_products
     }
 
     return context
