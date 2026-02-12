1cdaa496c0de522a82e6d6576bbf17ff6de0a4e8
Akaki Khorava <xoravaakaki989@gmail.com>
2026-02-12 16:27:48 +0100

    add str method to profile model

--- django_1/user/models.py
+++ django_1/user/models.py
@@ -7,3 +7,6 @@ class ProfileModel(models.Model):
     phone_number = PhoneNumberField(region='GE')
     address = models.CharField(max_length=200, null=True, blank=True)
     birth_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.user.username} - profile'
