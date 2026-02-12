83298926004f5d69e52b2161b9ddefe4c7ad9633
Akaki Khorava <xoravaakaki989@gmail.com>
2026-02-12 16:37:57 +0100

    in profile model changed foreign key to one to one field

--- django_1/user/models.py
+++ django_1/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 class ProfileModel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = PhoneNumberField(region='GE')
     address = models.CharField(max_length=200, null=True, blank=True)
     birth_date = models.DateField()
