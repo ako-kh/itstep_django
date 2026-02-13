3cdb3b7ca57a2b2c9da275f7b4d058bc453cbf06
Akaki Khorava <xoravaakaki989@gmail.com>
2026-02-13 17:08:15 +0100

    add signe out view

--- django_1/user/urls.py
+++ django_1/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SigneUpView, SigneInView
+from .views import SigneUpView, SigneInView, SigneOutView
 
 app_name = 'user'
 
 urlpatterns = [
     path('signe_up/', SigneUpView.as_view(), name='signe_up'),
     path('signe_in/', SigneInView.as_view(), name='signe_in'),
+    path('signe_out/', SigneOutView.as_view(), name='signe_out'),
 ]
--- django_1/user/views.py
+++ django_1/user/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.views.generic import (
     CreateView,
 )
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import ProfileCreationForm
 from .models import ProfileModel
 
@@ -27,4 +27,7 @@ class SigneUpView(CreateView):
 
 class SigneInView(LoginView):
     template_name = 'signe_in.html'
+    next_page = '/'
+
+class SigneOutView(LogoutView):
     next_page = '/'
