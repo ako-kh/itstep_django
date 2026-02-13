320184ee45301f163eef5dcbe03993e8f9c36c49
Akaki Khorava <xoravaakaki989@gmail.com>
2026-02-13 16:48:46 +0100

    add signe up page

--- django_1/user/urls.py
+++ django_1/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SigneUpView
+from .views import SigneUpView, SigneInView
 
 app_name = 'user'
 
 urlpatterns = [
-    path('signe_up/', SigneUpView.as_view(), name='signe_up')
+    path('signe_up/', SigneUpView.as_view(), name='signe_up'),
+    path('signe_in/', SigneInView.as_view(), name='signe_in'),
 ]
--- django_1/user/views.py
+++ django_1/user/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.views.generic import (
     CreateView,
 )
+from django.contrib.auth.views import LoginView
 from .forms import ProfileCreationForm
 from .models import ProfileModel
 
@@ -23,3 +24,7 @@ class SigneUpView(CreateView):
         )
         profile.save()
         return super().form_valid(form)
+
+class SigneInView(LoginView):
+    template_name = 'signe_in.html'
+    next_page = '/'
