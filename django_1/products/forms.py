4a0c0bc9c3f890d76a62d94e7bccefd33cea6074
Akaki Khorava <xoravaakaki989@gmail.com>
2026-02-15 22:08:40 +0100

    exclude wishlist from forms

--- django_1/products/forms.py
+++ django_1/products/forms.py
@@ -4,9 +4,9 @@ from .models import Product
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        exclude = ['is_available']
+        exclude = ['is_available', 'wishlist']
 
 class UpdateProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ['wishlist']
